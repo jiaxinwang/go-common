c13c541e7d6af214a40bafdf3dab1a1923725a11
jiaxinwang <jiaxinwang@me.com>
2021-07-20 11:42:17 +0800

    + http

--- mail/mail.go
+++ mail/mail.go
@@ -17,14 +17,20 @@ type Config struct {
 }
 
 // Send ...
-func Send(config Config, from, to, subject, test, html string) error {
+func Send(config Config, from, to, subject, text, html string) error {
 	e := email.NewEmail()
 	e.From = from
 	e.To = []string{to}
 	e.Subject = subject
-	e.Text = []byte(test)
-	e.HTML = []byte(html)
-	err := e.Send(config.SMTPAddr, smtp.PlainAuth(config.Identity, config.UserName, config.Password, config.Host))
+	// e.Subject = `subject`
+	e.Text = []byte(text)
+	// e.HTML = []byte(html)
+	err := e.Send(
+		config.SMTPAddr,
+		smtp.PlainAuth(config.Identity, config.UserName, config.Password, config.Host),
+		// `smtp.163.com:25`,
+		// smtp.PlainAuth(``, `imbotbotbot@163.com`, `VNAKFMVXQOJGQNGK`, `smtp.163.com`),
+	)
 	if err != nil {
 		logrus.WithError(err).Error()
 	}
